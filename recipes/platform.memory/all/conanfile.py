7cbcf782a9639f3fe7f973adc656347a2ef7a254
FreePhoenix888 <freephoenix888@gmail.com>
2022-02-22 19:14:49 +0600

    Add ms-gsl dependency to platform.memory

--- recipes/platform.memory/all/conanfile.py
+++ recipes/platform.memory/all/conanfile.py
@@ -39,6 +39,7 @@ class PlatformInterfacesConan(ConanFile):
 
     def requirements(self):
         self.requires("platform.collections/0.1.0")
+        self.requires("ms-gsl/4.0.0")
 
     def validate(self):
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler))
