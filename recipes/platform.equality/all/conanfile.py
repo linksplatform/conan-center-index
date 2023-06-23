ac1b803e178ac3fe973ac01156cad4f2e23167a0
Daulet <uralskdev@gmail.com>
2023-06-24 00:29:16 +0500

    platform.equality translated to conan 2.0
    Signed-off-by: Daulet <uralskdev@gmail.com>

--- recipes/platform.equality/all/conanfile.py
+++ recipes/platform.equality/all/conanfile.py
@@ -1,66 +1,80 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import get, copy, download
+from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=1.33.0"
-
+required_conan_version = ">=1.52.0"
 
 class PlatformInterfacesConan(ConanFile):
     name = "platform.equality"
-    license = "LGPL-3.0-or-later"
-    homepage = "https://github.com/linksplatform/Equality"
+    description = """platform.equality is one of the libraries of the LinksPlatform modular framework, which uses
+    innovations from the C++20 standard, for slow parody any typing dictionary and others."""
+    license = "Unlicense"
     url = "https://github.com/conan-io/conan-center-index"
-    description = "platform.delegates is one of the libraries of the LinksPlatform modular framework, " \
-        "which uses innovations from the C++20 standard, for slow parody any typing dictionary and others."
-    topics = ("linksplatform", "cpp20", "equality", "ranges", "any", "header-only")
-    settings = "compiler"
+    homepage = "https://github.com/linksplatform/Equality"
+    topics = ("platform", "concepts", "header-only")
+    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
     @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def _subfolder_sources(self):
+        return os.path.join(self.source_folder, "cpp", "Platform.Equality")
 
     @property
-    def _internal_cpp_subfolder(self):
-        return os.path.join(self._source_subfolder, "cpp", "Platform.Equality")
+    def _min_cppstd(self):
+        return "20"
 
     @property
     def _compilers_minimum_version(self):
         return {
-            "gcc": "10",
+            "gcc": "11",
             "Visual Studio": "16",
-            "clang": "11",
-            "apple-clang": "11"
+            "msvc": "193",
+            "clang": "13",
         }
 
-    @property
-    def _minimum_cpp_standard(self):
-        return 20
-
-    def validate(self):
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler))
-
-        if not minimum_version:
-            self.output.warn("{} recipe lacks information about the {} compiler support.".format(
-                self.name, self.settings.compiler))
-
-        elif tools.Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration("platform.equality/{} "
-                                            "requires C++{} with {}, "
-                                            "which is not supported "
-                                            "by {} {}.".format(
-                self.version, self._minimum_cpp_standard, self.settings.compiler, self.settings.compiler,
-                self.settings.compiler.version))
-
-        if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, self._minimum_cpp_standard)
-
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
-
-    def package(self):
-        self.copy("*.h", dst="include", src=self._internal_cpp_subfolder)
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
+
+    def validate(self):
+        if self.settings.compiler == "apple-clang":
+            raise ConanInvalidConfiguration(f"{self.ref} requires C++{self._min_cppstd}, "
+                                            "which is not supported "
+                                            f"by {self.settings.compiler}.")
+
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
+
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(f"{self.ref} requires C++{self._min_cppstd}, "
+                                            "which is not supported "
+                                            "by {self.settings.compiler} {self.settings.compiler.version}.")
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version]["source"], strip_root=True)
+
+    def package(self):
+        copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        copy(
+                self,
+                pattern="*.h",
+                dst=os.path.join(self.package_folder, "include"),
+                src=os.path.join(self.source_folder, "cpp", "Platform.Equality"),
+            )
+    def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+
+        self.cpp_info.set_property("cmake_file_name", "Platform.Equality")
+        self.cpp_info.set_property("cmake_target_name", "Platform.Equality::Platform.Equality")
+
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
+        self.cpp_info.names["cmake_find_package"] = "Platform.Equality"
+        self.cpp_info.names["cmake_find_package_multi"] = "Platform.Equality"
